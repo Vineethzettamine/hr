6db37c8b4a8f7d96fa93ffbce080818be4ef9d1e
vineeth <vineeth.m@zettamine.com>
2024-03-25 18:29:57 +0530

    Ui issues on login page of birthday card and announcement card

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -73,7 +73,8 @@
                                 <img class="mt-2 mb-2" width="230px" src="{{ asset('assets/images/zettamine-transparent.png') }}" alt="">
                                 <div class="envelope-image-bg">
                                     <h6 class="announcement-heading">Announcement</h6>
-                                    <p class="announcement-para">Exciting News! We are thrilled to announce the launch <br> of our latest product [Product Name]. It's designed to <br> [mention key features/benefits]. Get ready for an <br> unparalleled experience!</p>
+                                    <p class="announcement-para">Exciting News! We are thrilled to announce the launch <br> of our latest product [Product Name]. It's designed to <br> [mention key features/benefits]. Get ready for an <br> unparalleled experience!
+                                </p>
                                 </div>
 
 
@@ -116,11 +117,11 @@
                                     </div>
                                     <div class="col-md-8 container">
                                         <div id="carouselExampleIndicators" class="carousel slide">
-                                            <div class="carousel-indicators">
+                                            <!-- <div class="carousel-indicators">
                                                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                                                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                                                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                                            </div>
+                                            </div> -->
                                             <div class="carousel-inner">
                                                 @foreach($birthday_users as $birthday_user)
                                                 <div class="carousel-item active">
@@ -132,14 +133,7 @@
                                                 </div>
                                                 @endforeach
                                             </div>
-                                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                                <span class="visually-hidden">Previous</span>
-                                            </button>
-                                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                                <span class="visually-hidden">Next</span>
-                                            </button>
+                                           
                                         </div>
                                     </div>
 
